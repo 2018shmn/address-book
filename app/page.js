7e80d75561cfbcb8e7386d2e64fea135ce3b5c8b
2018Shmn <2018Shmn@gmail.com>
2025-05-25 13:07:37 -0500

    firebase update

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 'use client'
 
 import React, { useEffect, useState } from "react";
-import { auth, db, storage } from "../firebase_config";
+import { auth, db, storage } from "../firebase";
 import {
     collection,
     addDoc,
--- firebase.js
+++ firebase.js
@@ -4,13 +4,13 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAr73mSGTiQKQ4QjlJ1AvQP-aT3cBlkHrI",
-  authDomain: "inventory-management-c47cc.firebaseapp.com",
-  projectId: "inventory-management-c47cc",
-  storageBucket: "inventory-management-c47cc.appspot.com",
-  messagingSenderId: "828515117493",
-  appId: "1:828515117493:web:59e70f151aa325881291fc",
-  measurementId: "G-QP36V9M7CV"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
 };
 
 const app = typeof window !== "undefined" ? initializeApp(firebaseConfig) : null;
